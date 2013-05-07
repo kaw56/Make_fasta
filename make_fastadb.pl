a9d22e951ca1a1aa54dc7e5d89fa3aebb0cabf79
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-07 15:58:32 +0100

    Pseudocode
    Outline for a script that takes plain sequence and a list of gene names and
    their start and stop points in the sequence and splits the sequence into
    entries in a fasta database flat files to make it a little easier to compare
    where things hit.

---
+++ make_fastadb.pl
@@ -0,0 +1,47 @@
+#!usr/bin/perl
+# make_fastadb.pl
+use warnings;
+use strict;
+
+use Bio::SeqIO;
+use Bio::Tools::Run::StandAloneBlastPlus;
+
+# make sure that this is just a little reusable..
+
+die "usage: make_fastadb.pl <sequence file> <annotation file>" unless (@ARGV == 2);
+
+my $seq_filename    = shift;
+my $anno_filename   = shift;
+
+open(my $seq_file, "<", $seq_filename) or die "can't open file $seq_filename, $!";
+
+# read it into an array
+
+my @seq;
+
+while (my $line = <$seq_file>) {
+    if ($line =~ /^>/) {
+        next;
+    } else {
+        chomp $line;        
+        push(@seq, $line);
+    } 
+}
+
+# make the array a string
+
+my $genome = join( "", @seq);
+
+# this is just to test that my sequence is fine
+
+
+
+
+# take the sequence annotation file I made and make it a hash? 
+
+# open an output file
+
+# for each annotation
+#   write "> <the name of the gene> <gene length>"
+#   write the sequence between the start and stop (remember to adjust for perl counting from 0 rather than 1) on the next line
+
