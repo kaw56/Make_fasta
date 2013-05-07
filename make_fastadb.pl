54f45dacd7f529bd93184d4333ad44c605099b4f
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-07 16:55:29 +0100

    adjustment to comments
    added a comment line to explain the if statement in the while loop
    for reading in the mitochondrial genome sequence

--- make_fastadb.pl
+++ make_fastadb.pl
@@ -16,6 +16,7 @@ open(my $seq_file, "<", $seq_filename) or die "can't open file $seq_filename, $!
 my @seq;
 
 while (my $line = <$seq_file>) {
+    # if the first line starts with a > skip that line
     if ($line =~ /^>/) {
         next;
     } else {
@@ -27,38 +28,8 @@ while (my $line = <$seq_file>) {
 # make the array a string
 my $genome = join( "", @seq);
 
-#################################################
-# this is just to test that my sequence is fine #
-#################################################
 
-use Bio::SeqIO;
-use Bio::Tools::Run::StandAloneBlastPlus;
 
-# open file for reading as a sequence file
-my $seq_in = Bio::SeqIO->new(-format => 'fasta',
-                             -file   => $seq_filename);
-my $seq1 = $seq_in->next_seq();
-
-# need to have my string looking like fasta format
-my $genome2 = ">mitogenome\n" . $genome;
-
-# need to do some perl magic to make it deal with the string properly
-open(my $stringfh, "<", \$genome2) or die "Could not open string for reading: $!";
-
-my $seqio = Bio::SeqIO-> new(-fh     => $stringfh,
-                             -format => 'fasta');
-my $seq2 = $seqio->next_seq();
-
-# run bl2seq
-my $factory = Bio::Tools::Run::StandAloneBlastPlus->new(-db_name => 'testdb',
-                                                        -create => 1);
-
-$factory->bl2seq(   -method  => 'blastn',
-                    -query   => $seq1,
-                    -subject => $seq2,
-                    -outfile => 'bl2seq-out',);
-
-$factory->cleanup;
 
 # take the sequence annotation file I made and make it a hash? 
 
